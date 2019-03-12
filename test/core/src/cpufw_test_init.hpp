1659ee0e71b3540d62d3ce415b9c2ffa6954aad3
Ian A.B. King <iansens@hotmail.com>
2019-03-12 21:11:20 +0100

    roomba

--- test/core/src/cpufw_test_init.hpp
+++ test/core/src/cpufw_test_init.hpp
@@ -11,10 +11,10 @@
 										[[maybe_unused]] auto& inst_map = vfw.inst_map; \
 										mem.zero_memory(); \
 										regs.zero_registers(true);\
-										const registers::flag FLAG_SUB = registers::flag::SUB;\
-										const registers::flag FLAG_ZERO = registers::flag::ZERO;\
-										const registers::flag FLAG_HCARRY = registers::flag::HALF_CARRY;\
-										const registers::flag FLAG_CARRY = registers::flag::CARRY
+										[[maybe_unused]] const registers::flag FLAG_SUB = registers::flag::SUB;\
+										[[maybe_unused]] const registers::flag FLAG_ZERO = registers::flag::ZERO;\
+										[[maybe_unused]] const registers::flag FLAG_HCARRY = registers::flag::HALF_CARRY;\
+										[[maybe_unused]] const registers::flag FLAG_CARRY = registers::flag::CARRY
 
 /* Initialize common cpu firmware unit test variables [sys,regs,mem,inst_map] */
 #define CPUFW_SLEEPY_TESTINIT() _CPUFW_SLEEPYTEST_INIT_VARS()
