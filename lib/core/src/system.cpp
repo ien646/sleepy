70f6775d19990db4568658438f48587f97ba5401
Ian A.B. King <iansens@hotmail.com>
2019-03-23 03:04:28 +0100

    time 'accurate' VBLANK

--- lib/core/src/system.cpp
+++ lib/core/src/system.cpp
@@ -1,6 +1,7 @@
 #include <sleepy/system.hpp>
 
 #include <iostream>
+#include <chrono>
 
 namespace sleepy
 {
@@ -11,16 +12,31 @@ namespace sleepy
 
     void system::start()
     {
+        using clock = std::chrono::high_resolution_clock;
         while (true)
-        {
+        {            
+            auto pre_tick_time = clock::now();
             uint64_t next_vblank_cycles = 
                 _vcpu.elapsed_cycles() + CYCLES_PER_VBLANK;
             while(_vcpu.elapsed_cycles() <= next_vblank_cycles)
             {
                 _vcpu.exec_next_tick();
             }
-            std::cout << "[ESEC: ]" << elapsed_seconds() << "VBLANK" << std::endl;
+            auto post_tick_time = clock::now();
+            std::cout << "[ESEC: " << elapsed_seconds() << "] VBLANK" << std::endl;
+
+            auto spin_time = std::chrono::duration_cast<std::chrono::nanoseconds>(post_tick_time - pre_tick_time);
+            
+            // SPINWAIT FOR VBLANK
+            while(spin_time.count() < NANOS_PER_VBLANK) 
+            {
+                post_tick_time = clock::now();
+                spin_time = std::chrono::duration_cast<std::chrono::nanoseconds>(post_tick_time - pre_tick_time);
+            }
+
+            /*--------------------*/
             // VBLANK TRIGGER
+            /*--------------------*/
         }
     }
 
